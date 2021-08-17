d99246aa1ae84c35f611ccbfacb5e6a95ffe06d2
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-08-17 12:54:12 +0530

    vue3-getting-started for Chip

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<ejs-chiplist id="chip">
+    <e-chips>
+        <e-chip text="Andrew"></e-chip>
+        <e-chip text="Janet"></e-chip>  
+        <e-chip text="Laura"></e-chip>
+        <e-chip text="Margaret"></e-chip>
+    </e-chips>
+</ejs-chiplist>
+</template>
+
+<script>
+import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+import "../node_modules/@syncfusion/ej2-vue-buttons/styles/material.css";
+import { ChipListComponent, ChipsDirective, ChipDirective } from "@syncfusion/ej2-vue-buttons";
+
+export default {
+    name: "App",
+    components: {
+      "ejs-chiplist": ChipListComponent,
+      "e-chips": ChipsDirective,
+      "e-chip": ChipDirective
+    }
+}
+</script>
